16e09420df5da51614f9da0426923f7aaca956bb
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2020-12-09 16:09:57 -0500

    update setup.bash

--- Simulation/devel/setup.bash
+++ Simulation/devel/setup.bash
@@ -6,3 +6,7 @@ CATKIN_SHELL=bash
 # source setup.sh from same directory as this file
 _CATKIN_SETUP_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
 . "$_CATKIN_SETUP_DIR/setup.sh"
+source /opt/ros/noetic/setup.bash
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
